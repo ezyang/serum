d738c80d5687c5c8552a723a62964ac525e3f538
Edward Z. Yang <ezyang@cs.stanford.edu>
2015-06-01 15:20:40 -0700

    Final notes.
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- Main.hs
+++ Main.hs
@@ -20,25 +20,27 @@ generateBalancedTree3 lvls = loop 0 lvls
    loop !k 0 = Leaf k
    loop !k n = Tree (loop k (n-1)) (loop (k+1) (n-1))
 
+-- See README for some information:
 main = do
     args <- getArgs
-    let (treedepth, rest) =
+    let (d, rest) =
             case args of
                 n:rest -> (read n :: Int, rest)
                 _ -> error "requires one argument of tree depth"
-    constTree <- evaluate (force (generateBalancedTree3 treedepth))
+        mkTree = evaluate (force (generateBalancedTree3 d))
     withArgs rest $
       defaultMain $
-        [ env (return constTree) $ \tree -> bench "Ser/deser with binary" $
-            nf (isTree . B.decode . B.encode) tree
-        , env (return treedepth) $ \d -> bench "Compute tree, Ser/deser with binary" $
-            nf (isTree . B.decode . B.encode . generateBalancedTree3) d
-        , env (return constTree) $ \tree -> bench "Copy tree" $
-            nf (isTree . cheneycopy) tree
-        , env (return treedepth) $ \d -> bench "Compute tree, Copy tree" $
-            nf (isTree . cheneycopy . generateBalancedTree3) d
-        , env (return constTree) $ \tree -> bench "Copy tree, Ser/deser with binary" $
-            nf (isTree . B.decode . B.encode . cheneycopy) tree
-        , env (return treedepth) $ \d -> bench "Compute tree, Copy tree, Ser/deser with binary" $
+        tail [ undefined -- just to get leading commas
+        , bench "genE" $
+            nf (isTree                                    . generateBalancedTree3) d
+        , bench "gen-copyE" $
+            nf (isTree                       . cheneycopy . generateBalancedTree3) d
+        , env mkTree $ \t -> bench "gen-copyF" $
+            nf (isTree                       . cheneycopy) t
+        , bench "gen-binaryE" $
+            nf (isTree . B.decode . B.encode              . generateBalancedTree3) d
+        , bench "gen-copy-binaryE" $
             nf (isTree . B.decode . B.encode . cheneycopy . generateBalancedTree3) d
+        , env mkTree $ \t -> bench "gen-copy-binaryF" $
+            nf (isTree . B.decode . B.encode . cheneycopy) t
         ]
